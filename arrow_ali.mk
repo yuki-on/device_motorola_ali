580a495468ca48e48fea2303422b5dca3d7a14f7
Yuki <99554811+yuki-on@users.noreply.github.com>
2023-07-01 13:38:50 -0300

    Update and rename aosp_ali.mk to arrow_ali.mk

--- arrow_ali.mk
+++ arrow_ali.mk
@@ -17,14 +17,14 @@
 $(call inherit-product, device/motorola/ali/device.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+DEVICE_MAINTAINER := Lipe
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_NAME := aosp_ali
+PRODUCT_NAME := arrow_ali
 PRODUCT_DEVICE := ali
 PRODUCT_MODEL := moto g(6)
 
